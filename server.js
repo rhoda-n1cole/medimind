81ccd47b22fec387a516e60efd0ee21397b693f2
rhoda-n1cole <r.umutesi1@alustudent.com>
2026-07-30 22:02:52 +0200

    Redirect root URL to login page

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.redirect("/login.html");
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/auth", authRoutes);
